41edd55bdc32c8b8614065c5143c35611522cf4c
yoshiki <yoshiki0911@gmail.com>
2020-05-28 23:12:53 +0900

    queue_clientのテスト構造を他のclientと統一

--- test/test_queue_client.py
+++ test/test_queue_client.py
@@ -2,15 +2,17 @@ import pytest
 from azfs.clients.queue_client import AzQueueClient
 
 
+credential = ""
+queue_client = AzQueueClient(credential=credential)
+test_queue_path = "https://test.queue.core.windows.net/test_queue/"
+
+
 def test_not_implemented_error():
-    credential = ""
-    path = "https://test.queue.core.windows.net/"
-    queue_client = AzQueueClient(credential=credential)
     with pytest.raises(NotImplementedError):
-        queue_client.info(path=path)
+        queue_client.info(path=test_queue_path)
 
     with pytest.raises(NotImplementedError):
-        queue_client.rm(path=path)
+        queue_client.rm(path=test_queue_path)
 
     with pytest.raises(NotImplementedError):
-        queue_client.get_container_client_from_path(path=path)
+        queue_client.get_container_client_from_path(path=test_queue_path)
