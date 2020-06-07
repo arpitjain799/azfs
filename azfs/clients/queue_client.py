dfb81e928be613496d0bb6b5f20aeaa7179167f2
yoshiki <yoshiki0911@gmail.com>
2020-06-07 14:08:14 +0900

    #32 base64のencodeとdecodeの追加

--- azfs/clients/queue_client.py
+++ azfs/clients/queue_client.py
@@ -2,6 +2,7 @@ from azfs.clients.client_interface import ClientInterface
 from typing import Union
 from azure.identity import DefaultAzureCredential
 from azure.storage.queue import QueueClient
+import base64
 
 
 class AzQueueClient(ClientInterface):
@@ -50,6 +51,8 @@ class AzQueueClient(ClientInterface):
         received_message = next(message_itr)
         message_id = received_message['id']
         pop_receipt = received_message['pop_receipt']
+        # decode with base64
+        received_message['content'] = base64.b64decode(received_message['content'].encode('utf-8')).decode('utf-8')
 
         # delete message in queue
         if delete_after_receive:
@@ -57,7 +60,11 @@ class AzQueueClient(ClientInterface):
         return received_message
 
     def _put(self, path: str, data):
-        return self.get_file_client_from_path(path).send_message(data)
+        # encode with base64
+        encoded_data = base64.b64encode(data.encode('utf-8')).decode('utf-8')
+        put_data = self.get_file_client_from_path(path).send_message(encoded_data)
+        put_data['content'] = base64.b64decode(put_data['content'].encode('utf-8')).decode('utf-8')
+        return put_data
 
     def _info(self, path: str):
         """
