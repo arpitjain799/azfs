f923b27935781e81c071d1fb73b3574773d1a0da
yoshiki <yoshiki0911@gmail.com>
2020-07-19 21:17:34 +0900

    #72 AzFileSystemの作成

---
+++ azfs/az_file_system.py
@@ -0,0 +1,43 @@
+from fsspec import AbstractFileSystem
+from fsspec.spec import AbstractBufferedFile
+
+from .core import AzFileClient
+
+
+class AzFileSystem(AbstractFileSystem):
+
+    def __init__(self, *args, **storage_options):
+        super().__init__(*args, **storage_options)
+
+    def ls(self, path, detail=True, **kwargs):
+        pass
+
+    def copy(self, path1, path2, **kwargs):
+        pass
+
+    def _rm(self, path):
+        pass
+
+    def created(self, path):
+        pass
+
+    def modified(self, path):
+        pass
+
+
+class AzFile(AbstractBufferedFile):
+
+    def __init__(
+            self,
+            fs,
+            path,
+            mode="rb",
+            block_size="default",
+            autocommit=True,
+            cache_type="readahead",
+            cache_options=None,
+            **kwargs):
+        super().__init__(fs, path, mode, block_size, autocommit, cache_type, cache_options, **kwargs)
+
+    def _fetch_range(self, start, end):
+        pass
