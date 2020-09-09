ba8245b7baf8e1ca4b0c8326e0da8433f739a06a
yoshiki <yoshiki0911@gmail.com>
2020-09-09 23:58:38 +0900

    update version

--- azfs/__init__.py
+++ azfs/__init__.py
@@ -7,6 +7,6 @@ from azfs.az_file_system import AzFileSystem
 from azfs.utils import BlobPathDecoder
 
 # comparable tuple
-VERSION = (0, 2, 2)
+VERSION = (0, 2, 3)
 # generate __version__ via VERSION tuple
 __version__ = ".".join(map(str, VERSION))
