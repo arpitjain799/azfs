992dec215c98c5c2a658e30e2eb8b17cbb980a4d
yoshiki <yoshiki0911@gmail.com>
2020-12-13 01:45:56 +0900

    #121 add cmd.decorator example

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,9 +1,9 @@
-from azfs.cli.constants import WELCOME_PROMPT, MOCK_FUNCTION
+from azfs.cli.constants import WELCOME_PROMPT
 from click.testing import CliRunner
 from azfs.cli import cmd
 
 
-def test_cli_output():
+def test_cmd():
     result = CliRunner().invoke(cmd)
     # result.stdout
     assert result.stdout == f"{WELCOME_PROMPT}\n"
