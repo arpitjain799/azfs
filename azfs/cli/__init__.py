0fa68f905c82fad263a257f21f8a9ae2b1825a59
yoshiki <yoshiki0911@gmail.com>
2021-01-12 00:08:37 +0900

    #138 refactor: use function_manipulation

--- azfs/cli/__init__.py
+++ azfs/cli/__init__.py
@@ -1,12 +1,11 @@
-from inspect import signature
 import os
-import re
 from typing import List
 
 import click
 import azfs
 from .factory import CliFactory
 from .constants import WELCOME_PROMPT, MOCK_FUNCTION
+from azfs.function_manipulation import get_signature_and_additional_imports
 
 
 @click.group(invoke_without_command=True)
@@ -49,80 +48,14 @@ def _write_az_file_client_content(az_file_client_content: List[str]):
 
 
 def _load_functions(export_decorator) -> (int, List[str]):
-
-    def _decode_types(input_str: str):
-        pattern = r"(<module '(?P<module_name>[A-Za-z0-9]+)' from '.+?'>)?(<class '(?P<class_name>.*?)'>)?"
-        result = re.match(pattern, input_str)
-        if not result:
-            return "", None
-        else:
-            result_dict = result.groupdict()
-            return _get_module_and_imports(**result_dict)
-
-    def _get_module_and_imports(module_name: str, class_name: str) -> (str, str):
-        if module_name is not None:
-            return module_name, module_name
-        elif class_name is not None:
-            if "." in class_name:
-                import_str = class_name.split(".", 1)
-                return class_name, import_str[0]
-            else:
-                return class_name, ""
-        else:
-            raise ValueError
-
-    def _decode_signature(input_str: str):
-        module_pattern = r"<module '(?P<module_name>[A-Za-z0-9]+)' from '.+?'>"
-        class_pattern = r"<class '(?P<class_name>.*?)'>"
-
-        result = re.sub(module_pattern, r"\g<module_name>", input_str)
-        result = re.sub(class_pattern, r"\g<class_name>", result)
-        return result
-
     new_lines = []
     append_functions = len(export_decorator.functions)
 
     for f in export_decorator.functions:
-        # initialize
-        additional_import_list = []
-
+        # function name
         function_name = f['register_as']
-        sig = signature(f['function'])
-
-        # argument parameters
-        for signature_params in sig.parameters:
-            annotation_candidate = sig.parameters[signature_params].annotation
-            _, import_candidate = _decode_types(str(annotation_candidate))
-            additional_import_list.append(import_candidate)
-
-        # return parameters
-        return_annotation = sig.return_annotation
-        return_annotation_type = type(return_annotation)
-        if str(return_annotation) == "<class 'inspect._empty'>":
-            # inspect._empty is not accessible
-            pass
-        elif str(return_annotation_type) == "<class 'typing._GenericAlias'>":
-            # typing._GenericAlias is also not accessible
-            pass
-        elif return_annotation_type == tuple:
-            for signature_params in return_annotation:
-                _, import_candidate = _decode_types(str(signature_params))
-                additional_import_list.append(import_candidate)
-        else:
-            _, import_candidate = _decode_types(str(return_annotation))
-            additional_import_list.append(import_candidate)
-
-        ideal_sig = _decode_signature(str(sig))
-        if "()" in ideal_sig:
-            ideal_sig = ideal_sig.replace(")", "**kwargs)", 1)
-        else:
-            ideal_sig = ideal_sig.replace(")", ", **kwargs)", 1)
-
-        # create additional import
-        additional_import_candidate = [s for s in set(additional_import_list) if len(s) > 0]
-        additional_import = ""
-        if len(additional_import_candidate) > 0:
-            additional_import = f"import {', '.join(additional_import_candidate)}\n"
+        # get additional import and ideal signature
+        additional_import, ideal_sig = get_signature_and_additional_imports(f['function'])
 
         # create mock function
         new_mock_function: str = MOCK_FUNCTION % (additional_import, function_name, ideal_sig)
